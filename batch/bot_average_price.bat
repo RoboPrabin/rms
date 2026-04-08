f23e9c8e4f85970b681b23cdbb04ec2ec43a18eb
RoboPrabin <prabinchand333@gmail.com>
2026-04-08 17:19:20 +0545

    Add batch files

---
+++ batch/bot_average_price.bat
@@ -0,0 +1,10 @@
+@echo off
+title Average Price Bot
+
+REM Change to the project directory
+cd /d "E:\Trishakti\Projects\rms"
+
+REM Run the login page script with Streamlit
+py "average_price_scrapper.py"
+
+exit
